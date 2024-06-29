8e67c37923d6dca2c69674420012581abac9d4bb
x <>
2024-06-29 13:26:46 -0700

    timing

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ setTimeout(() => {
     start();
 }, 300);
 
+const scanner = new jscanify();
 
 const start = () => {
-    var scanner = new jscanify();
-    var paperWidth = 1000/(2**0.5)
-    var paperHeight = 1000;
-    var resultCanvas = scanner.extractPaper(image, paperWidth, paperHeight);
+    scan();
+};
+
+const scan = () => {
+    const stime = performance.now();
+    const paperWidth = 1000/(2**0.5)
+    console.log('a:', performance.now() - stime);
+    const paperHeight = 1000;
+    console.log('b:', performance.now() - stime);
+    const resultCanvas = scanner.extractPaper(image, paperWidth, paperHeight);
+    console.log('c:', performance.now() - stime);
     document.body.appendChild(resultCanvas);
-}
+    console.log('d:', performance.now() - stime);
+    const delta = performance.now() - stime;
+    console.log('Time:', delta);
+};
