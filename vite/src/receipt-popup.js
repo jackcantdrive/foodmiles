bff32419e7cb2936864f1edd231e742716564afd
x <>
2024-06-29 23:28:42 -0700

    add connecting wallet

--- vite/src/receipt-popup.js
+++ vite/src/receipt-popup.js
@@ -32,7 +32,7 @@
 
 
 
-// import { DAppKitUI } from '@vechain/dapp-kit-ui';
+import { DAppKitUI } from '@vechain/dapp-kit-ui';
 
 // document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 //   <div class="container">
@@ -44,49 +44,49 @@
 //         </div>
 // `;
 
-// const walletConnectOptions = {
-//     projectId: 'a0b855ceaf109dbc8426479a4c3d38d8',
-//     metadata: {
-//         name: 'Sample VeChain dApp',
-//         description: 'A sample VeChain dApp',
-//         url: window.location.origin,
-//         icons: [`${window.location.origin}/images/logo/my-dapp.png`],
-//     },
-// };
+const walletConnectOptions = {
+    projectId: 'a0b855ceaf109dbc8426479a4c3d38d8',
+    metadata: {
+        name: 'Sample VeChain dApp',
+        description: 'A sample VeChain dApp',
+        url: window.location.origin,
+        icons: [`${window.location.origin}/images/logo/my-dapp.png`],
+    },
+};
 
-// const vechainDAppKitOptions = {
-//     nodeUrl: 'https://testnet.vechain.org/',
-//     genesis: 'test',
-//     walletConnectOptions,
-//     usePersistence: true,
-// };
+const vechainDAppKitOptions = {
+    nodeUrl: 'https://testnet.vechain.org/',
+    genesis: 'test',
+    walletConnectOptions,
+    usePersistence: true,
+};
 
-// DAppKitUI.configure(vechainDAppKitOptions);
+DAppKitUI.configure(vechainDAppKitOptions);
 
-// // custom button configuration
+// custom button configuration
 
-// const customButton = document.getElementById('custom-button');
+const customButton = document.getElementById('custom-button');
 
-// if (customButton) {
-//     customButton.addEventListener('click', () => {
-//         DAppKitUI.modal.open();
-//     });
+if (customButton) {
+    customButton.addEventListener('click', () => {
+        DAppKitUI.modal.open();
+    });
 
-//     const handleConnected = (address) => {
-//         if (address) {
-//             const formattedAddress = `${address.slice(0, 6)}...${address.slice(
-//                 -4,
-//             )}`;
-//             customButton.innerText = `Disconnect from ${formattedAddress}`;
-//         } else {
-//             customButton.innerText = 'Connect Custom Button';
-//         }
-//     };
+    const handleConnected = (address) => {
+        if (address) {
+            const formattedAddress = `${address.slice(0, 6)}...${address.slice(
+                -4,
+            )}`;
+            customButton.innerText = `Disconnect from ${formattedAddress}`;
+        } else {
+            customButton.innerText = 'Connect Custom Button';
+        }
+    };
 
-//     handleConnected(DAppKitUI.wallet.state.address);
+    handleConnected(DAppKitUI.wallet.state.address);
 
-//     DAppKitUI.modal.onConnectionStatusChange(handleConnected);
-// }
+    DAppKitUI.modal.onConnectionStatusChange(handleConnected);
+}
 
 
 
